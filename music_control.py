e48a013d0bf13f3a43b2f1c5fff904cd452d946f
Katarzyna Koprowska <38463698+KatKopr@users.noreply.github.com>
2018-11-14 15:44:26 +0100

    Add files via upload

--- music_control.py
+++ music_control.py
@@ -7,6 +7,7 @@ class Music_Control:
     def __init__(self):
         self.playing=True
         pygame.mixer.music.load("music.ogg")
+        pygame.mixer.music.set_volume(0.1)
     
     def play(self):
         pygame.mixer.music.play(loops=-1)
